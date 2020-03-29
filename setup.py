6ff8f45534c2304dc4613f09db5efa2ecdcb5b9f
Starr Yang <starryangt@gmail.com>
2020-03-29 18:33:59 -0400

    base package setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="python-epub-writer",
+    version="0.0.1",
+    author="Starr Yang",
+    author_email="starryangt@gmail.com",
+    description="Package for writing epubs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/starryangt/python-epub-writer",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
