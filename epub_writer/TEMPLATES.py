4c6a61589afc2f45a61f15bb2b4a75a8d2b370a5
Starr Yang <starryangt@gmail.com>
2020-03-30 21:00:13 -0400

    Added some of the XML templates

---
+++ epub_writer/TEMPLATES.py
@@ -0,0 +1,45 @@
+
+COVER = """
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <title>Cover</title>
+    <style type="text/css"> img { max-width: 100%; } </style>
+  </head>
+  <body>
+      <img src="../Images/Cover.jpg"/>
+  </body>
+</html>
+"""
+
+PAGE = """
+<?xml version="1.0" encoding="utf-8" standalone="no"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+            <title>${title}</title>
+    </head>
+    <body>
+        <h3 align="right">${title}</h3>
+        ${body}
+    </body>
+</html>
+"""
+
+CONTENT_OPF = """
+<?xml version="1.0" encoding="UTF-8"?>
+<package xmlns="http://www.idpf.org/2007/opf"
+         version="3.0"
+         unique-identifier="BookId"
+         xmlns:dc="http://purl.org/dc/elements/1.1/"
+         xmlns:dcterms="http://purl.org/dc/terms/"
+         xml:lang="en"
+         xmlns:media="http://www.idpf.org/epub/vocab/overlays/#"
+         prefix="ibooks: http://vocabulary.itunes.apple.com/rdf/ibooks/vocabulary-extensions-1.0/">
+
+        <metadata xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:opf="http://www.idpf.org/2007/opf">
+        <dc:identifier id="BookId">${UUID}</dc:identifier>
+
+
+</package>
+"""
